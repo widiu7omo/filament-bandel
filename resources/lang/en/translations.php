6169882f554beb0747468c938cfb1d72dd199055
mohamedsabil83 <me@mohamedsabil83.com>
2023-04-13 04:48:44 +0300

    [en]: Fix typo

--- resources/lang/en/translations.php
+++ resources/lang/en/translations.php
@@ -4,5 +4,5 @@ return [
     'single-ban-success' => 'Model was banned!',
     'single-unban-success' => 'Model was unbanned!',
     'bulk-ban-success' => 'Models ware banned!',
-    'bulk-unban-success' => 'Models were banned!'
+    'bulk-unban-success' => 'Models were unbanned!'
 ];
